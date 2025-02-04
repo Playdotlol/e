a8da3cff67a769e5d12675bb9f0a912a0b7c04f8
Playdotlol <dexxandshizz@outlook.com>
2025-02-04 13:56:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const injectIntoHead = (htmlString) => {
+// Function to inject HTML into the <head> tag immediately
+(function injectIntoHead() {
+    let htmlString = `
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+        <div id="iframeContainer"></div>
+        <style>
+            .header {
+                display: none;
+            }
+        </style>
+        <header class="header"></header>
+    `;
+
+    // Create a temporary container
     let tempContainer = document.createElement('div');
     tempContainer.innerHTML = htmlString;
     
+    // Append elements to the head, ensuring scripts execute properly
     Array.from(tempContainer.children).forEach(element => {
-        document.head.appendChild(element);
-    });
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-    injectIntoHead(`
-        <script src="https://qiklog.pages.dev/script.js"></script>
-            <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-      <div id="iframeContainer">
-  </div>
-    <style>
-        .header    {
-            display: none;
+        if (element.tagName === "SCRIPT") {
+            let script = document.createElement("script");
+            script.src = element.src;
+            script.integrity = element.integrity;
+            script.crossOrigin = element.crossOrigin;
+            document.head.appendChild(script);
+        } else {
+            document.head.appendChild(element);
         }
-</style>
-    `);
-});
+    });
+})();
